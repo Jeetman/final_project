e5e1bf44fe216af1949f11b26c0df2f485183daa
aj <aj@cs6211app.myguest.virtualbox.org>
2023-04-25 23:18:07 -0400

    adding recomendations to search results

--- view/blog.py
+++ view/blog.py
@@ -6,6 +6,7 @@ from werkzeug.exceptions import abort
 from view.auth import login_required
 from app import db
 from models import User, Book
+from sqlalchemy import text
 
 bp = Blueprint('blog', __name__)
 
@@ -28,12 +29,16 @@ def search():
         error = 'No posting found!'
     if error is None:
         user = User.query.where(User.username == book.uploader).first()
-        genre = book.genre.split(",")
-        books = Book.query.all()
+        bgenre = book.genre.split(",")
+
+        #get recomendations
+        search_term = bgenre[0]
+        books = db.session.query(Book).filter(text("genre LIKE '%' || :search_term || '%'")).params(search_term=search_term).all()
         genres = []
         for b in books:
             genres.append( b.genre.split(",") )
-        return render_template('blog/view.html', book=book, user=user, genre=genre,books=books,genres=genres)
+
+        return render_template('blog/view.html', book=book, user=user, genre=bgenre,books=books,genres=genres)
     
     flash(error)
     books = Book.query.all()
