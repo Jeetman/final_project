384923d13dd435352ca227b7019c56efb1a3269b
aj <aj@cs6211app.myguest.virtualbox.org>
2023-04-25 23:10:00 -0400

    adding recomendations to search results

--- migrations/versions/d0c7b8e4b57c_initial_migration.py
+++ migrations/versions/d0c7b8e4b57c_initial_migration.py
@@ -36,7 +36,6 @@ def upgrade():
 
 
 def downgrade():
-    op.drop_table('post')
     op.drop_table('books')
     op.drop_table('user')
 
--- view/blog.py
+++ view/blog.py
@@ -28,8 +28,12 @@ def search():
         error = 'No posting found!'
     if error is None:
         user = User.query.where(User.username == book.uploader).first()
-        genres = book.genre.split(",")
-        return render_template('blog/view.html', book=book, user=user, genres=genres)
+        genre = book.genre.split(",")
+        books = Book.query.all()
+        genres = []
+        for b in books:
+            genres.append( b.genre.split(",") )
+        return render_template('blog/view.html', book=book, user=user, genre=genre,books=books,genres=genres)
     
     flash(error)
     books = Book.query.all()
@@ -64,48 +68,3 @@ def create():
             return redirect(url_for('blog.index'))
 
     return render_template('blog/create.html')
-
-def get_post(id, check_author=True):
-
-    post = Post.query \
-        .join(User) \
-        .filter(Post.id == id) \
-        .with_entities(Post.id, Post.title, Post.body, Post.created, Post.author_id, User.username) \
-        .first()
-
-    if post is None:
-        abort(404, f"Post id {id} doesn't exist.")
-
-    if check_author and post.author_id != g.user.id:
-        abort(403)
-
-    return post
-
-@bp.route('/<int:id>/update', methods=('GET', 'POST'))
-@login_required
-def update(id):
-    post = Post.query.filter_by(id=id).first()
-    if request.method == 'POST':
-        title = request.form['title']
-        body = request.form['body']
-        error = None
-
-        if not title:
-            error = 'Title is required.'
-
-        if error is not None:
-            flash(error)
-        else:
-            post.title = title
-            post.body = body
-            db.session.commit()
-            return redirect(url_for('blog.index'))
-
-    return render_template('blog/update.html', post=post)
-@bp.route('/<int:id>/delete', methods=('POST',))
-@login_required
-def delete(id):
-    post = Post.query.filter_by(id=id).first()
-    db.session.delete(post)
-    db.session.commit()
-    return redirect(url_for('blog.index'))
